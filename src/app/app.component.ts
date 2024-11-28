fbf7def7bac20bddf63378a836d7d953e7cbd42b
xPrismatico <srfuentesavila@gmail.com>
2024-11-28 19:07:55 -0300

    feat: add base folders, flowbite and tailwind install

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { OnInit } from '@angular/core';
+import { initFlowbite } from 'flowbite';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'Rick-and-morty-Frontend';
+  ngOnInit(): void {
+    initFlowbite();
+  }
 }
